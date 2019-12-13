bfcf4555bc1f4618e03237b453e59f990333b51e
nick <gnn@mm.st>
2019-12-13 12:05:16 +0300

    add project

---
+++ src/main/java/ru/javamentor/predProject9/predProject9/PredProject9Application.java
@@ -0,0 +1,11 @@
+package ru.javamentor.predProject9.predProject9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredProject9Application {
+	public static void main(String[] args) {
+		SpringApplication.run(PredProject9Application.class, args);
+	}
+}
---
+++ src/test/java/ru/javamentor/predProject9/predProject9/PredProject9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javamentor.predProject9.predProject9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredProject9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
